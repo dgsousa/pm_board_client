3dfdfa0798ac99fc87596eec65aaa69ae6e94dd1
dousa <dsousa@adobe.com>
2019-07-26 20:21:43 -0700

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 
 const config = {
-  entry: './src/Index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
