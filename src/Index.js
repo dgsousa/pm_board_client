97b82f0b73f86eaf9144672bb2f3c5c89fe6b5bf
dousa <dsousa@adobe.com>
2019-11-10 20:29:02 -0800

    udpates

--- src/Index.js
+++ src/Index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import io from 'socket.io-client';
-
 import createStoreWithMiddlewareAndSocket from './store/setupStore';
 import App from './App';
 
-
-const socket = io('localhost:3000');
-// socket.on('connect', () => console.log('socketConnected'));
-
+const socket = io('http://localhost:3000');
 const store = createStoreWithMiddlewareAndSocket(socket);
 
 ReactDOM.render(
